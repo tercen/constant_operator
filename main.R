96b265636751eba116aefd5d7ebb7e45aa3a303c
amaurel <alexandre.maurel@gmail.com>
2018-09-03 16:21:11 +0200

    tercen 0.7.1

--- main.R
+++ main.R
@@ -1,11 +1,12 @@
 library(tercen)
 library(dplyr)
- 
-fun = function(x) {return(x)} 
- 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(value = fun(as.double(ctx$op.value('value')))) %>%
+
+ctx = tercenCtx()
+
+value = as.double(ctx$op.value('value'))
+cTable = tibble(.ci=seq.int(0,ctx$cschema$nRows-1)) %>% mutate(value=value)
+rTable = tibble(.ri=seq.int(0,ctx$rschema$nRows-1)) %>% mutate(value=value)
+
+cTable %>% full_join(rTable) %>%
   ctx$addNamespace() %>%
-  ctx$save()
+  ctx$save() 
